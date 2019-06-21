ea5e773388832dbc298233997fee125ef3e0c856
Marcin Wiechnik <mwiechnik>
2019-06-21 20:51:32 +0200

    Add project files.

---
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Akka.Actor;
+using MovieStreaming.Messages;
+
+namespace MovieStreaming.Actors
+{
+    public class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine($"Creating {GetType().Name}");
+
+            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message), message => message.UserId >= 12);
+        }
+
+        private void HandlePlayMovieMessage(PlayMovieMessage message)
+        {
+            Console.WriteLine($"Received movie title '{message.MovieTitle}' from user id {message.UserId}");
+        }
+
+        protected override void PreStart()
+        {
+            Console.WriteLine("PlaybackActor: PreStart");
+            base.PreStart();
+        }
+
+        protected override void PostStop()
+        {
+            Console.WriteLine("PlaybackActor: PostStop");
+            base.PostStop();
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            Console.WriteLine("PlaybackActor: PreRestart, because: " + reason.Message);
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            Console.WriteLine("PlaybackActor: PostRestart, because: " + reason.Message);
+            base.PostRestart(reason);
+        }
+    }
+}
---
+++ MovieStreaming/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Actor.Internal;
+using MovieStreaming.Actors;
+using MovieStreaming.Messages;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            using (MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem"))
+            {
+                Console.WriteLine("Actor System created");
+
+                Props playbackActorProps = Props.Create<PlaybackActor>();
+
+                IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackActorProps, "OurPlaybackActor");
+
+                playbackActorRef.Tell(new PlayMovieMessage("Akka.NET: The Movie", 12));
+                playbackActorRef.Tell(new PlayMovieMessage("Partial Recall", 15));
+                playbackActorRef.Tell(new PlayMovieMessage("Boolean Lies", 77));
+                playbackActorRef.Tell(new PlayMovieMessage("Conan the Destroyer", 123));
+
+                playbackActorRef.Tell(PoisonPill.Instance);
+
+                Console.ReadLine();
+
+                MovieStreamingActorSystem.Terminate();
+                Console.WriteLine("Actor System terminating");
+                MovieStreamingActorSystem.WhenTerminated.Wait();
+                Console.WriteLine("Actor System terminated");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
